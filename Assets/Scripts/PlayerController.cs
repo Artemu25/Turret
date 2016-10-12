33e90c7e336d5ba426eddbb3550da8c029e1c514
Artem <artemu25@mail.ru>
2016-10-12 17:20:26 +0300

    Walk with gun

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public CharacterController controller;
+    public float move_speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        if (controller.isGrounded)
+        {
+            controller.Move(transform.forward * vertical * move_speed * Time.deltaTime);
+            controller.Move(transform.right * horizontal * move_speed * Time.deltaTime);
+        }
+        else
+            controller.Move(Physics.gravity * Time.deltaTime);
+    }
+}
