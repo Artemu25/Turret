bc384cd518537d56280bcd384e99be8e71abd703
Artem <artemu25@mail.ru>
2016-10-12 21:19:45 +0300

    Laser gun

---
+++ Assets/RayCast.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayCast : MonoBehaviour {
+
+    private LineRenderer laserLine;
+    public Camera camera;
+    private float nextFire;
+    public Transform gunEnd;
+
+    // Use this for initialization
+    void Start () {
+        laserLine = GetComponent<LineRenderer>();
+	}
+	
+    public void Shoot()
+    {
+        if (Time.time > nextFire)
+        {
+            nextFire = Time.time + 0.25f;
+            StartCoroutine(ShotEffect());
+            Vector3 rayOrigin = camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+            RaycastHit hit;
+            laserLine.SetPosition(0, gunEnd.position);
+            if (Physics.Raycast(rayOrigin, camera.transform.forward, out hit))
+            {
+                laserLine.SetPosition(1, hit.point);
+            }
+            else
+            {
+                laserLine.SetPosition(1, rayOrigin + (camera.transform.forward * 50));
+            }
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        Vector3 lineOrigin = camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+        Debug.DrawRay(lineOrigin, camera.transform.forward * 50, Color.green);
+    }
+
+    private IEnumerator ShotEffect()
+    {
+        laserLine.enabled = true;
+
+        yield return new WaitForSeconds(0.07f);
+
+        laserLine.enabled = false;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,24 @@ public class PlayerController : MonoBehaviour {
 
     public CharacterController controller;
     public float move_speed;
+    private float currentY;
+    public float speedRotate = 180;
+    public float minY = -20;
+    public float maxY = 20;
+    public RayCast gun;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start () {
+        currentY = Camera.main.transform.eulerAngles.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         float vertical = Input.GetAxis("Vertical");
         float horizontal = Input.GetAxis("Horizontal");
+        float mx = Input.GetAxis("Mouse X");
+        float my = Input.GetAxis("Mouse Y");
         if (controller.isGrounded)
         {
             controller.Move(transform.forward * vertical * move_speed * Time.deltaTime);
@@ -22,5 +30,17 @@ public class PlayerController : MonoBehaviour {
         }
         else
             controller.Move(Physics.gravity * Time.deltaTime);
+        transform.Rotate(transform.up * mx * speedRotate * Time.deltaTime);
+        currentY = Mathf.Clamp(currentY - my * speedRotate * Time.deltaTime, minY, maxY);
+        Vector3 camrot = Camera.main.transform.rotation.eulerAngles;
+        Camera.main.transform.rotation = Quaternion.Euler(currentY, camrot.y, camrot.z);
+        //Camera.main.transform.Rotate(Camera.main.transform.right * my * speedRotate * Time.deltaTime);
+
+
+        if (Input.GetMouseButton(0))
+        {
+            gun.Shoot();
+
+        }
     }
 }
